ce9cdd889286370be46cf3e9a249734a98f5cc64
Thomas Ford <thomasford3344@gmail.com>
2026-06-22 10:19:56 -0500

    feat: added template 5

--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -15,6 +15,7 @@ import {
   ResumePDFTemplate2,
   ResumePDFTemplate3,
   ResumePDFTemplate4,
+  ResumePDFTemplate5,
 } from './templates';
 import { OpenAIService } from '../openai/openai.service';
 
@@ -2962,6 +2963,9 @@ CANDIDATE_BACKGROUND:
       return template.generate();
     } else if (templateName === 'template4') {
       const template = new ResumePDFTemplate4(data);
+      return template.generate();
+    } else if (templateName === 'template5') {
+      const template = new ResumePDFTemplate5(data);
       return template.generate();
     }
 
